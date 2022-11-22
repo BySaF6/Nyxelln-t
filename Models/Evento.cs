c7acfa46bfa7ded447c3d8dfcfa1658739dfa9a4
Samuel <samarandaf6@gmail.com>
2022-11-22 19:38:04 +0100

    Inicializada clase eventos y creado fichero json para almacenar un primer evento.

--- Models/Evento.cs
+++ Models/Evento.cs
@@ -4,24 +4,42 @@ namespace Nyxellnt.Models
 {
     class Evento
     {
-        public int idEvento = 0;
+        public string idEvento;
+        public static int eventNumber = 1;
         public string nombre;
         public string cantante;
         public string descripcion;
         public string localidad;
+        public DateTime fecha;
         public decimal precioEntrada;
         public int stock;
 
         //Constructor
-        public Evento(int idEvento, string nombre, string cantante, string descripcion, string localidad, decimal precioEntrada, int stock)
+        public Evento(int idEvento, string nombre, string cantante, string descripcion, string localidad, DateTime fecha, decimal precioEntrada, int stock)
         {
-            this.idEvento = idEvento;
+            this.idEvento = eventNumber.ToString();
+            eventNumber++;
             this.nombre = nombre;
             this.cantante = cantante;
             this.descripcion = descripcion;
             this.localidad = localidad;
+            this.fecha = fecha;
             this.precioEntrada = precioEntrada;
             this.stock = stock;
+        }
+
+        public void listarEventoLinea(){
+            Console.WriteLine(idEvento+" "+nombre + " " + cantante + " "+localidad+" "+precioEntrada);
+        }
+
+        public void listarEventoExtendido(){
+            Console.WriteLine(nombre);
+            Console.WriteLine(cantante);
+            Console.WriteLine(descripcion);
+            Console.WriteLine(localidad);
+            Console.WriteLine(fecha);
+            Console.WriteLine(precioEntrada);
+            Console.WriteLine(stock);
         }
     }
 }
